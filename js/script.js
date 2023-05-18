a114f126880d21c5e517f11eee7a2cfa01112b20
ShabatinEvg <106232874+ShabatinEvg@users.noreply.github.com>
2023-05-18 15:42:07 +0300

    images-main__item

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ document.addEventListener('click', documentClick);
 
 function documentClick(e) {
     const targetItem = e.target;
+
     if (targetItem.closest('.icon-menu')) {
-        document.documentElement.classList.toggle('menu--open');
+        document.documentElement.classList.toggle('menu-open');
     }
 }
